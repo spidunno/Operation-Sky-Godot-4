e67f2cc8912b1422cf47caaf0a1fb8f5806c16f7
spidunno <unnospid@gmail.com>
2023-03-20 15:39:55 -0400

    -Made world spawn in MultiplayerSpawner -Made lava chunks synchronize in multiplayer -Made island positions synchronize (nothing else about the islands sync)

--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -2,10 +2,10 @@ extends Node3D
 
 const SAVE_PATH = "user://save_config_file.ini"
 
-@onready var username = $CanvasLayer/Control/VBoxContainer/Username
-@onready var address = $CanvasLayer/Control/VBoxContainer/ServerMenu/Address
-@onready var port = $CanvasLayer/Control/VBoxContainer/ServerMenu/Port
-@onready var thememenu = $CanvasLayer/Control/VBoxContainer/Theme
+@onready var username = $Title/CanvasLayer/Control/VBoxContainer/Username
+@onready var address = $Title/CanvasLayer/Control/VBoxContainer/ServerMenu/Address
+@onready var port = $Title/CanvasLayer/Control/VBoxContainer/ServerMenu/Port
+@onready var thememenu = $Title/CanvasLayer/Control/VBoxContainer/Theme
 
 func save():
 	var config = ConfigFile.new()
@@ -14,14 +14,34 @@ func save():
 	config.set_value("User", "port", port.value)
 	config.set_value("User", "address", address.text)
 	config.save(SAVE_PATH)
+func change_scene(path):
+	var world = $World
+	var scene = load(path).instantiate()
+	world.add_child(scene)
 
 func _on_Host_pressed():
 	save()
-	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(port.value)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer server.")
+		return
+	multiplayer.multiplayer_peer = peer
+	$Title.queue_free()
+	change_scene("res://Scenes/World.tscn")
+#	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 
 func _on_Join_pressed():
 	save()
-	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(address.text, port.value)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer client.")
+		return
+	multiplayer.multiplayer_peer = peer
+	$Title.queue_free()
+#	change_scene("res://Scenes/World.tscn")
+#	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 	
 
 func _on_Quit_pressed():
@@ -36,11 +56,11 @@ func _ready():
 	thememenu.add_item("Swift")
 	thememenu.add_item("Clean")
 	thememenu.selected = config.get_value("User", "themeindex", 0)
-	$CanvasLayer/Control.theme = load("res://Assets/Resources/"+thememenu.get_item_text(thememenu.selected)+".tres")
+	$Title/CanvasLayer/Control.theme = load("res://Assets/Resources/"+thememenu.get_item_text(thememenu.selected)+".tres")
 	username.text = config.get_value("User", "name", "")
 
 func _on_Theme_item_selected(index):
-	$CanvasLayer/Control.theme = load("res://Assets/Resources/"+thememenu.get_item_text(index)+".tres")
+	$Title/CanvasLayer/Control.theme = load("res://Assets/Resources/"+thememenu.get_item_text(index)+".tres")
 	var config = ConfigFile.new()
 	config.set_value("User", "themeindex", index)
 	config.save(SAVE_PATH)
